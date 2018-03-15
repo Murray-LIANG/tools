faad9ff9cb511f319d5196c5a5b3e18757215658
Ryan Liang <ryan.liang@emc.com>
2018-03-15 15:55:46 +0800

    Modify to sleep 2 second

--- time_attach_skip_hlu_0.py
+++ time_attach_skip_hlu_0.py
@@ -6,8 +6,9 @@ import sys
 from functools import wraps
 from multiprocessing import dummy as multithread
 from time import time
+from contextlib import contextmanager
 
-# import mock
+import mock
 import storops
 from tooz import coordination
 
@@ -36,12 +37,29 @@ def to_str(obj):
     return str(obj)
 
 
+@contextmanager
+def timer():
+    class _Time(object):
+        def __init__(self, time_start):
+            self.start = time_start
+            self.end = None
+
+        @property
+        def interval(self):
+            return self.end - self.start
+
+    _timer = _Time(time())
+    try:
+        yield _timer
+    finally:
+        _timer.end = time()
+
+
 def prepare_host_luns():
     hosts = [host for host in unity.get_host()
-             if host.get_id() in ('Host_147', 'Host_148', 'Host_149',
-                                  'Host_150', 'Host_166')]
+             if host.name.startswith('host-liangr-')]
     luns = [lun for lun in unity.get_lun()
-            if lun.get_id() not in ('sv_999', 'sv_1061', 'sv_1062')]
+            if lun.get_id() not in ('sv_999', 'sv_1061', 'sv_1062')][:29]
     n = int(math.ceil(len(luns) / len(hosts)))
     hosts_repeat = itertools.chain.from_iterable([hosts] * n)
     return zip(hosts_repeat, luns)
@@ -61,27 +79,33 @@ def attach_lock(coordinator, host, lun):
         host.attach(lun, skip_hlu_0=True)
 
 
-@timing
-def attach(host, lun):
-    # with mock.patch('storops.unity.resource.host.UnityHost.attach',
-    #                 new=fake_attach):
-    #     host.attach(lun)
-    host.attach(lun, skip_hlu_0=True)
+def attach(index, host, lun):
+    import time as t
+    t.sleep(index * 2)
+    with timer() as t:
+        # with mock.patch('storops.unity.resource.host.UnityHost.attach',
+        #                 new=fake_attach):
+        #     host.attach(lun)
+        host.attach(lun, skip_hlu_0=True)
+    print('Attach: {},{},{}'.format(host.get_id(), lun.get_id(), t.interval))
 
 
 def main(action):
     host_lun_pairs = prepare_host_luns()
     print('Number of host-lun pairs: {}'.format(len(host_lun_pairs)))
-    pool = multithread.Pool(10)
-    if action == 'test':
-        coordinator = coordination.get_coordinator('file:///tmp/skip_hlu_0',
-                                                   'localhost')
-        coordinator.start()
-        # pool.map(lambda t: attach_lock(coordinator, *t), host_lun_pairs)
-        pool.map(lambda t: attach(*t), host_lun_pairs)
-        coordinator.stop()
-    elif action == 'clean':
-        pool.map(lambda t: t[1].detach_from(None), host_lun_pairs)
+
+    pool = multithread.Pool(29)
+    with timer() as total_time:
+        if action == 'test':
+            # coordinator = coordination.get_coordinator(
+            #     'file:///tmp/skip_hlu_0', 'localhost')
+            # coordinator.start()
+            # pool.map(lambda t: attach_lock(coordinator, *t), host_lun_pairs)
+            pool.map(lambda t: attach(t[0], *t[1]), enumerate(host_lun_pairs))
+            # coordinator.stop()
+        elif action == 'clean':
+            pool.map(lambda t: t[1].detach_from(None), host_lun_pairs)
+    print('Total time: {}'.format(total_time.interval))
 
 
 if __name__ == '__main__':
